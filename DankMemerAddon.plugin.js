88d99a3df2e9e70782a6a96b35a531dbb6b715bd
Aperre <60411288+Aperre@users.noreply.github.com>
2021-11-29 02:24:37 +0100

    ss

--- DankMemerAddon.plugin.js
+++ DankMemerAddon.plugin.js
@@ -3,7 +3,7 @@
  * @displayName DankMemerHelper
  * @source https://raw.githubusercontent.com/Aperre/BetterDiscord/master/DankMemerAddon.plugin.js
  * @authorId 547077385225502736
- * @version 1.0.3
+ * @version 1.0.4
  */
  /*@cc_on
  @if (@_jscript)
@@ -33,7 +33,7 @@
       "name": "DankMemerHelper",
       "authors": [{"name": "Ape", "github_username": "Aperre", "twitter_username": "ApeTheMonk", "discord_id": "226677096091484160"}],
       "description": "Adds Features That will help you with Dank Memer",
-      "version": "1.0.3",
+      "version": "1.0.4",
       "github": "https://github.com/Aperre/BetterDiscord",
       "github_raw": "https://raw.githubusercontent.com/Aperre/BetterDiscord/master/DankMemerAddon.plugin.js"
     },
@@ -383,7 +383,7 @@
   						id: "test",
   						label: "Say Test",
   						action: () => {
-  							ZLibrary.sendMessage("Test")
+  							ZLibrary.DiscordAPI.currentChannel.sendMessage("Test")
   						}
   					}));
   				}
